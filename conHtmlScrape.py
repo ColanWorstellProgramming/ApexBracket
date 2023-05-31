4abed5f18efc522c5aaaec08be0d68b439220e12
Colan <cworstellprogramming@gmail.com>
2023-05-31 02:07:21 -0500

    Concurrent not working yet

--- conHtmlScrape.py
+++ conHtmlScrape.py
@@ -2,9 +2,8 @@
 import pandas as pd
 import requests
 import time
-import asyncio
-import aiohttp
 import numpy as np
+import concurrent.futures
 
 urls = {
     "RankScore": "https://apex.tracker.gg/apex/leaderboards/stats/all/RankScore",
@@ -19,86 +18,96 @@ urls = {
 }
 
 country = "us"
-pages = 100
-output_file = "1000.csv"
-request_delay = 2
+pages = 10
+output_file = "10k.csv"
+request_delay = 2  # Delay between requests in seconds
+requests_per_interval = 30  # Number of requests per interval
+interval = 60  # Interval duration in seconds
+max_attempts = 3  # Maximum number of retry attempts
 
 dataframes = {}
+total_time_waited = 0
+requests_made = 0
+start_time = time.time()
+
+def fetch_data(url):
+    attempts = 0
+    while attempts < max_attempts:
+        response = requests.get(url)
+        if response.status_code == 200:
+            try:
+                dfs = pd.read_html(response.content)
+                if dfs:
+                    return dfs[0]
+            except ValueError as e:
+                print(f"Error parsing HTML for URL: {url}")
+        print(f"Retrying... Attempt {attempts + 1}")
+        time.sleep(15)
+        attempts += 1
+    print(f"Failed to retrieve data for {url}. Skipping all pages for this URL.")
+    return None
 
 for stat, url in urls.items():
     dataframes[stat] = pd.DataFrame()
 
-total_time_waited = 0
-start_time = time.time()
-
-async def fetch(session, url):
-    async with session.get(url) as response:
-        return await response.text()
-
-async def process_page(session, page):
+def process_page(page):
+    global requests_made
     print(f"Collecting data from page {page}/{pages}...")
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for stat, url in urls.items():
+            if url in dataframes[stat].columns:
+                continue  # Skip the URL if already fetched
 
-    tasks = []
-    for stat, url in urls.items():
-        print(f" - Collecting data for {stat}...")
-        task = asyncio.create_task(fetch(session, f"{url}?country={country}&page={page}"))
-        tasks.append(task)
+            print(f" - Collecting data for {stat}...")
+            table = None
+            url_failed = False
 
-    responses = await asyncio.gather(*tasks)
-
-    for response, (stat, url) in zip(responses, urls.items()):
-        try:
-            dfs = pd.read_html(response)
-            if dfs:
-                table = dfs[0]
-                table = table.replace('-', '')
-                table = table.replace([np.inf, -np.inf], np.nan)
-
-                if page == 1:
-                    dataframes[stat] = table
+            while table is None:
+                if requests_made < requests_per_interval:
+                    requests_made += 1
+                    futures.append(executor.submit(fetch_data, f"{url}?page={page}&country={country}"))
                 else:
-                    dataframes[stat] = pd.concat([dataframes[stat], table], ignore_index=True)
-            else:
-                print(f"No tables found for {stat} on page {page}")
-        except ValueError:
-            print(f"No tables found for {stat} on page {page}")
+                    print("Reached request limit for current interval. Waiting...")
+                    time.sleep(interval)
+                    requests_made = 0
+                    total_time_waited += interval
 
-    end_time = time.time()
-    time_taken = end_time - start_time
-    global total_time_waited
-    total_time_waited += time_taken
-    print(f"Completed page {page}/{pages} - Data collected (Time taken: {time_taken:.2f} seconds)\n")
+                if total_time_waited >= interval:
+                    total_time_waited = 0
+                    break
 
-async def main():
-    async with aiohttp.ClientSession() as session:
-        await asyncio.gather(*[process_page(session, page) for page in range(1, pages+1)])
+        for future in concurrent.futures.as_completed(futures):
+            table = future.result()
+            if table is not None:
+                stat = table.columns[0]
+                table.columns = table.columns.droplevel(0)
+                table = table.replace([-np.inf, np.inf], np.nan)
+                dataframes[stat] = pd.concat([dataframes[stat], table])
 
-# Run the event loop
-asyncio.run(main())
+    time.sleep(request_delay)
 
-# Merge all dataframes based on matching player name
+for page in range(1, pages + 1):
+    process_page(page)
+
+print("Merging dataframes...")
 merged_df = dataframes["RankScore"]
 for stat in dataframes.keys():
-    if stat != "RankScore" and "Player" in dataframes[stat].columns:
-        if stat in merged_df.columns:
-            merged_df = merged_df.merge(dataframes[stat][['Player', stat]], on='Player', how='left')
-
-# Drop the "Unnamed: 2" column if it exists
-merged_df = merged_df.drop("Unnamed: 2", axis=1, errors="ignore")
-
-# Convert float columns to integers
-for stat in dataframes.keys():
     if stat != "RankScore":
-        if stat in merged_df.columns:
-            if stat == "Kills":
-                merged_df[stat] = merged_df[stat].replace(np.nan, -1).astype(int)
-            else:
-                merged_df[stat] = merged_df[stat].fillna(-1).astype(int)
+        if 'Player' in merged_df.columns and 'Player' in dataframes[stat].columns:
+            merged_df = merged_df.merge(dataframes[stat], on="Player", how="left")
 
-# Save the merged dataframe to the output file
+# Convert float columns to nullable integers
+for stat in dataframes.keys():
+    if stat != "RankScore" and stat in merged_df.columns:
+        if stat == "Kills":
+            merged_df[stat] = merged_df[stat].replace(np.nan, pd.NA).astype("Int64")
+        else:
+            merged_df[stat] = merged_df[stat].fillna(pd.NA).astype("Int64")
+
+print("Writing merged dataframe to file...")
 merged_df.to_csv(output_file, index=False)
 
-total_time = time.time() - start_time
-print(f"Data saved to {output_file}")
-print(f"Total time taken: {total_time:.2f} seconds")
-print(f"Total time waited due to delay: {total_time_waited:.2f} seconds")
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"Data collection and merging completed in {execution_time:.2f} seconds.")
